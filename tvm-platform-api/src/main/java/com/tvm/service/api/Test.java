fa6aa8885d6621b9c10befb8ebf48ba14d6f03ae
tiancl <tcl3492@126.com>
2014-07-06 11:43:00 +0800

    first commit

---
+++ tvm-platform-api/src/main/java/com/tvm/service/api/Test.java
@@ -0,0 +1,9 @@
+package com.tvm.service.api;
+
+public class Test {
+
+	public static String builder(String str){
+		System.out.println(str);
+		return str;
+	}
+}
---
+++ tvm-platform-controller/src/main/java/com/tvm/test/crontroller/TVMIndex.java
@@ -0,0 +1,17 @@
+package com.tvm.test.crontroller;
+
+import com.tvm.service.api.Test;
+
+public class TVMIndex {
+
+	
+
+	public static void sendText() {
+	    Test	test = new Test();
+		System.out.print(test.builder("hello"));
+	}
+	public static void main(String args[]){
+		TVMIndex index=new TVMIndex();
+		index.sendText();
+	}
+}
