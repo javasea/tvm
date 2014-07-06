93db8dc6c8d6eb33dd0c589c60149ea4d6033299
tiancl <tcl3492@126.com>
2014-07-06 12:21:02 +0800

    hello git

--- tvm-platform-controller/src/main/java/com/tvm/test/crontroller/TVMIndex.java
+++ tvm-platform-controller/src/main/java/com/tvm/test/crontroller/TVMIndex.java
@@ -3,9 +3,6 @@ package com.tvm.test.crontroller;
 import com.tvm.service.api.Test;
 
 public class TVMIndex {
-
-	
-
 	public static void sendText() {
 	    Test	test = new Test();
 		System.out.print(test.builder("hello"));
@@ -13,5 +10,6 @@ public class TVMIndex {
 	public static void main(String args[]){
 		TVMIndex index=new TVMIndex();
 		index.sendText();
+		System.out.println("git is ok");
 	}
 }
